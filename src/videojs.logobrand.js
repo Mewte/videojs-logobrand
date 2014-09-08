61ce616ec1b6273fc782f4587f0aea6c0ab2f7e8
Dirk-jan <dirkjan@sanoweb.nl>
2014-09-08 12:09:50 +0200

    Moved styling to .css file, removed hide/show events, added hiding transition

--- src/videojs.logobrand.js
+++ src/videojs.logobrand.js
@@ -24,31 +24,10 @@
 			image.id = 'vjs-logobrand-image';
 			//image.style.height = settings.height;
 			//image.style.width = settings.width;
-			image.style.position = "absolute";
-			image.style.top = "4px";
-			image.style.right = "4px";
 			image.src = settings.image;
 		link.appendChild(image);
 		player.el().appendChild(link);
-			
-		var useractive = true;
-		player.on('useractive', function(){
-			useractive = true;
-			image.style.display = "";
-		});
-		player.on('userinactive', function(){
-			useractive = false;
-			if (!player.paused()){ //do not hide if player is paused
-				image.style.display = "none";
-			}
-		});
-		player.on('pause', function(){
-			image.style.display = "";
-		});
-		player.on('play', function(){
-			if (!useractive) //Don't hide image while user is active i.e. right when play is clicked
-				image.style.display = "none";
-		});
+		
 		this.loadImage = function(src){
 			document.getElementById("vjs-logobrand-image").src=src;
 		};
